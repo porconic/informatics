06a77478bc34500dd0bb018905cf6789bf8f82e7
porconic <117679434+porconic@users.noreply.github.com>
2022-12-19 12:47:51 +0100

    Add files via upload

---
+++ notes/Notes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/* 17.12.2022
+class A {
+private:
+	int x;
+public:
+	int get_x();
+}; 
+
+int A::get_x() {
+	return x;
+}
+
+int factorial_recursive(int x) {
+	if (x == 0)
+		return 1;
+	return x * factorial_recursive(x - 1);
+}
+
+int main(int argc, char* argv[]) {
+	A a1, a2, a3, a4;
+	A *p1 = &a1, *p2 = &a2, *p3 = &a3, *p4 = &a4;
+	A* v[] = { p1, p2, p3, p4 };
+	A c;
+	c.get_x();
+	A *p = &c;
+	p->get_x();
+	for (int i = 0; i < 4; i++) {
+		cout << v[i] << endl;
+	}
+}	*/
