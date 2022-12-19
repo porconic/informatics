5828c41a54f2291e0638d83b3da4542d1146c688
porconic <117679434+porconic@users.noreply.github.com>
2022-12-19 12:45:42 +0100

    Add files via upload

---
+++ array_plus/ArrayPlus.h
@@ -0,0 +1,11 @@
+class ArrayPlus{
+private:
+	int* v;
+	int size;
+public:
+	ArrayPlus(int*, int);
+	void PrintArrayPlus();
+	void RightShiftArrayPlus();
+	void SortArrayPlus();
+	int LinearSearchArrayPlus(int);
+};
